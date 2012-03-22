a021ad0816c2952f116d39422255e7a1db6d801f
Yiwu.zh15@gmail.com <Yiwu.zh15@gmail.com@e617b5ae-c912-3e10-b75e-17c95a945c33>
2012-03-22 15:52:09 +0000

    add

---
+++ src/hadoop/tools/Main.java
@@ -0,0 +1,83 @@
+package hadoop.tools;
+
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: yiwu
+ * Date: 12-3-22
+ * Time: 下午9:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+    // file format for example: D:/test/train
+    public static void trainSplit(String file, int split) {
+        Splitter splitter = SplitnMergeFactory.createSplitter(split, file, SplitnMergeFactory.SplitType.SEQUENCE);
+        try{
+            splitter.split();
+        } catch (IOException e) {
+            System.out.println("splitting training file error.");
+            e.printStackTrace();
+        }
+    }
+
+    // path format for example: D:/test/train | suffix = "model"
+    public static void trainMerge(String path, String suffix) {
+        Merger svmmerger = SplitnMergeFactory.createMerger(path, suffix, SplitnMergeFactory.MergeType.SVM);
+        svmmerger.merge();
+    }
+    
+    public static void testSplit(String file, int split) {
+        Splitter splitter = SplitnMergeFactory.createSplitter(split, file, SplitnMergeFactory.SplitType.SEQUENCE);
+        try{
+            splitter.split();
+        } catch (IOException e) {
+            System.out.println("splitting testing file error.");
+            e.printStackTrace();
+        }
+    }
+    
+    public static void testMerge(String path, String suffix) {
+        Merger merger = SplitnMergeFactory.createMerger(path, suffix);
+        merger.merge();
+    }
+    
+    public static void main(String[] args) {
+        if(args.length != 4) {
+            System.out.println("argments number error. please input 4 argments");
+            System.out.println("+\'a\' String int for trainSplit()");
+            System.out.println("+\'b\' String String for trainMerge()");
+            System.out.println("+\'c\' String int for testSplit()");
+            System.out.println("+\'d\' String String for testMerge()");
+            return;
+        }
+
+        char x = args[1].charAt(0);
+        switch (x) {
+            case 'a':
+                System.out.println("-->do trainSplit method with: "+args.toString());
+                trainSplit(args[2], Integer.valueOf(args[3]));
+                break;
+            case 'b':
+                System.out.println("-->do trainMerge method with: "+args.toString());
+                trainMerge(args[2], args[3]);
+                break;
+            case 'c':
+                System.out.println("-->do testSplit method with: "+args.toString());
+                testSplit(args[2], Integer.valueOf(args[3]));
+                break;
+            case 'd':
+                System.out.println("-->do testMerge method with: "+args.toString());
+                testMerge(args[2], args[3]);
+                break;
+            default:
+                System.out.println("error args[1], please instead it of \'a\' \'b\' \'c\' or \'d\'");
+                System.out.println("+\'a\' String int for trainSplit()");
+                System.out.println("+\'b\' String String for trainMerge()");
+                System.out.println("+\'c\' String int for testSplit()");
+                System.out.println("+\'d\' String String for testMerge()");
+                break;
+        }
+    }
+}
